6368b2ebc24bdb4a47e5acd7f354eb86f5975a38
Zaen67 <96893984+Zaen67@users.noreply.github.com>
2022-05-27 23:50:23 +0700

    .

---
+++ handlers/start.py
@@ -0,0 +1,49 @@
+import asyncio
+
+from helpers.filters import command
+from config import BOT_NAME as bn, BOT_USERNAME as bu, SUPPORT_GROUP, OWNER_USERNAME as me, START_IMG
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.delete()
+    await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
+    await message.reply_photo(
+        photo=f"{START_IMG}",
+        caption=f"""**━━━━━━━━━━━━━━━━━━
+✨ ʜᴇʏ {message.from_user.mention()} !
+
+        ᴛʜɪs ɪs [{bn}](t.me/{bu}), ᴀ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘ ᴠɪᴅᴇᴏᴄʜᴀᴛs...
+
+ᴀʟʟ ᴏꜰ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʜᴀɴᴅʟᴇʀs : ( `/ . • $ ^ ~ + * ?` )
+┏━━━━━━━━━━━━━━┓
+┣★
+┣★ ᴍᴀᴅᴇ ʙʏ: [𝐙𝐚𝐞𝐧](t.me/{me})
+┣★
+┗━━━━━━━━━━━━━━┛
+
+✨ ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴀʙᴏᴜᴛ ᴍᴇ ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ [ᴏᴡɴᴇʀ](t.me/{me}) ʙᴀʙʏ...
+━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "•ᴀᴅᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ•", url=f"https://t.me/{bu}?startgroup=true"
+                       ),
+                  ],[
+                    InlineKeyboardButton(
+                        "•ᴏᴡɴᴇʀ•", url=f"https://t.me/{me}"
+                    ),
+                    InlineKeyboardButton(
+                        "•sᴜᴘᴘᴏʀᴛ•", url=f"https://t.me/{SUPPORT_GROUP}"
+                    )
+                ],[
+                    InlineKeyboardButton(
+                        "•ɪɴʟɪɴᴇ•", switch_inline_query_current_chat=""
+               [                    
+           ]
+       ),
+    )
+
